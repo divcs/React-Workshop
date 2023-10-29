23a6a37e4252c04a47898e6cd06e562d5e8c82c1
Divya <singhdivya10aug2002@gmail.com>
2023-10-29 10:45:38 +0530

    Adding two numbers

--- src/App.js
+++ src/App.js
@@ -1,12 +1,46 @@
-import { Home } from "./pages/Home";
+import { useState } from 'react';
+import { Button, Form, InputGroup } from 'react-bootstrap';
+
 
 function App() {
   return (
+
+    const [fnum, setFnum]= useState();
+    const [snum, setSnum]= useState();
+    const [res, setRes]= useState();
+
+    const addFun=()=>{
+      setRes("Sum is "+(parseInt(fnum)+parseInt(snum)))
+    }
+    return 
+
     <>
-      <Home />
-      {/* <About/> */}
+      <InputGroup className="mb-3">
+        <InputGroup.Text id="basic-addon1">First Number</InputGroup.Text>
+        <Form.Control
+          placeholder="Enter first number"
+          aria-label="Username"
+          aria-describedby="basic-addon1"
+          value={fnum}
+          onChange={(e)=>{setFnum(e.target.value);}}
+        />
+      </InputGroup>
+
+      <InputGroup className="mb-3">
+        <InputGroup.Text id="basic-addon1">Second Number</InputGroup.Text>
+        <Form.Control
+          placeholder="Enter second number"
+          aria-label="Username"
+          aria-describedby="basic-addon1"
+          value={snum}
+          onChange={(e)=>{setFnum(e.target.value);}}
+        />
+      </InputGroup>
+
+      <Button variant="success" onClick={()=>addFun()}>ADD</Button>
+    <h1>Sum is: {res}</h1>
     </>
-  );
+  )
 }
 
 export default App;
